9d631826b585713d8a380a47c0b555d3134a938e
hirai-git <123721152+hirai-git@users.noreply.github.com>
2023-01-30 20:55:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ streamlit.dataframe(fruits_to_show)
 # Nes Section to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response
+streamlit.text(fruityvice_response)
