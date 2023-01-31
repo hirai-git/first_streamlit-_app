7b80a98236717a93dafcecc438314c07fc6a9197
hirai-git <123721152+hirai-git@users.noreply.github.com>
2023-01-31 22:17:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,17 +52,26 @@ try:
 except URLError as e:
     streamlit.error() 
         
-#don'st run anything past here while we troubleshoot
-streamlit.stop()
+
 
 
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains;")
-streamlit.dataframe(my_data_rows)
+streamlit.header("The fruit load list contains:")
+#snoflake-related funciton
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur
+        my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+        return my_cur.fetchall()
+
+#add a button to load the fruit
+if streamlit.botton('Get Fruit Load List')
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
+
+#don'st run anything past here while we troubleshoot
+streamlit.stop()
+
 #allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fuite would you like to add?','jackfruit')
 streamlit.write('Thank for adding ', add_my_fruit)
